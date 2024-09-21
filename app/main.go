249ef29327ff3b5a186b3acf118d0d0333501f5b
ChursinAlexUnder <keks63620@gmail.com>
2024-09-21 19:13:46 +0300

    Create local server

--- app/main.go
+++ app/main.go
@@ -2,9 +2,30 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+	"text/template"
 )
 
+// обработка html
+func homePage(w http.ResponseWriter, r *http.Request) {
+	tmp, err := template.ParseFiles("../web/index.html")
+	if err != nil {
+		fmt.Fprintf(w, "Error: %v", err)
+		return
+	}
+	tmp.Execute(w, nil)
+}
+
+func handleRequest() {
+	// обработка css и js
+	http.Handle("/web/", http.StripPrefix("/web/", http.FileServer(http.Dir("../web/"))))
+	// обработка изображений
+	http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("../assets/"))))
+	// Отображение страниц
+	http.HandleFunc("/", homePage)
+	http.ListenAndServe(":8080", nil)
+}
+
 func main() {
-	fmt.Print("Hello world!")
-	fmt.Print("Неllo, It's Sania!")
+	handleRequest()
 }
