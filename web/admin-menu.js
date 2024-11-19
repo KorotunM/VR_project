9efb7b9208bb11529991472cbf757576e965b656
ChursinAlexUnder <keks63620@gmail.com>
2024-11-19 16:32:00 +0300

    Deleted js scripts

--- web/admin-menu.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const toggleMenuButton = document.getElementById('toggleMenu');
-    const sidebar = document.getElementById('sidebar');
-
-    toggleMenuButton.addEventListener('click', () => {
-        sidebar.classList.toggle('hidden');
-    });
-});
